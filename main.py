c3c4762ced3dc37daa9188c394923915855c984f
Michal Šutek <sutek.spsit@gmail.com>
2023-10-16 00:13:06 +0200

    Add function sucin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.math import sum_number
+from utils.math import *
 
 if __name__ == '__main__':
-    print(sum_number(3,4))
+    print(sum_number(3, 4))
+    print(sucin(3,  4))
     print("Hello, World!")
--- utils/math.py
+++ utils/math.py
@@ -1,2 +1,6 @@
 def sum_number(x, y):
     return x + y
+
+
+def sucin(x, y):
+    return x * y
